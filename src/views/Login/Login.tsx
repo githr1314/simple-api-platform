c49200cc0d7ed6e3efee77a5c4bbfe0de92c9ee3
herui <herui@csii.com.cn>
2020-05-07 17:44:31 +0800

    new login

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,34 +1,55 @@
 import * as React from 'react';
-import { Layout, Menu, Row, Col } from 'antd';
-import { HomeOutlined, AppstoreOutlined, ProfileOutlined } from '@ant-design/icons';
-const { Header, Footer, Sider, Content } = Layout;
+import Style from './style.scss';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { withRouter } from 'react-router-dom';
+
+const layout = {
+    labelCol: {
+        offset: 4,
+        span: 5,
+    },
+    wrapperCol: {
+      span: 8,
+    },
+};
+const tailLayout = {
+    wrapperCol: {
+        offset: 10,
+    },
+};
+
 
 class Login extends React.Component {
-    constructor(props:Object) {
+    constructor(props:any) {
         super(props)
     }
+
+    onFinish(values:any) {
+        this.props?.history.push('/main');
+    };
+    
     render():JSX.Element {
         return (
-            <Layout style={{
-                minHeight: '100%'
-            }}>
-                <Header>
-                    <span>登陆</span>
-                </Header>
-                <Layout>
-                    <Sider>
-                    </Sider>
-                    <Content>
-                        <span>contnet</span>
-                    </Content>
-                </Layout>
-                <Footer>
-                </Footer>
-            </Layout>
+            <div className={Style.bg}>
+                <div className={Style.login}>
+                    <div className={Style.title}>一个简单的API管理工具</div>
+                    <Form {...layout} name="basic" onFinish={this.onFinish.bind(this)}>
+                        <Form.Item label="用户名" name="username" rules={[{required: true, message: '请输入用户名',}]}>
+                            <Input size='small' />
+                        </Form.Item>
+
+                        <Form.Item label="密码" name="password" rules={[{required: true, message: '请输入密码'},]}>
+                            <Input.Password  size='small' />
+                        </Form.Item>
+
+                        <Form.Item {...tailLayout}>
+                            <Button type="primary" htmlType="submit">登 陆</Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+            </div>
         )
     }
 }
 
-
-
-export default Login;
+export default withRouter(Login);
